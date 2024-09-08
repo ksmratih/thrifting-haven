aa22549704935b57b0e3c0aa04ec95f9f6a9d201
ksmratih <ratihhanindyani@gmail.com>
2024-09-08 19:57:32 +0700

    PWS deployed

--- thrifting_haven/settings.py
+++ thrifting_haven/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-kgf6(g3wxjag2vq9lz-hes5hixdj8wdy4_7d$8$zhqerfpuxdu
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["localhost", "127.0.0.1"]
+ALLOWED_HOSTS = ["localhost", "127.0.0.1","kusuma-ratih-thriftinghaven.pbp.cs.ui.ac.id"]
 
 
 # Application definition
