305bc2aafe84ab8da878fda492cbfd1118ff67b1
ksmratih <ratihhanindyani@gmail.com>
2024-09-11 23:37:40 +0700

    added assignment 3

--- main/views.py
+++ main/views.py
@@ -1,11 +1,44 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect   # Add import redirect at this line
+from main.forms import ThriftEntryForm
+from main.models import Product
+from django.http import HttpResponse
+from django.core import serializers
 
 # Create your views here.
 def show_main(request):
+    thrift_entries = Product.objects.all()
+
     context = {
         'application_name' : 'thrifting-haven',
         'class': 'PBD',
-        'name': 'Kusuma Ratih Hanindyani'
+        'name': 'Kusuma Ratih Hanindyani',
+        'product_entries' : thrift_entries,
     }
 
     return render(request, "main.html", context)
+
+def create_product(request):
+    form = ThriftEntryForm(request.POST or None)
+
+    if form.is_valid() and request.method == "POST":
+        form.save()
+        return redirect('main:show_main')
+
+    context = {'form': form}
+    return render(request, "create_product.html", context)
+
+def show_xml(request):
+    data = Product.objects.all()
+    return HttpResponse(serializers.serialize("xml", data), content_type="application/xml")
+
+def show_json(request):
+    data = Product.objects.all()
+    return HttpResponse(serializers.serialize("json", data), content_type="application/json")
+
+def show_xml_by_id(request, id):
+    data = Product.objects.filter(pk=id)
+    return HttpResponse(serializers.serialize("xml", data), content_type="application/xml")
+
+def show_json_by_id(request, id):
+    data = Product.objects.filter(pk=id)
+    return HttpResponse(serializers.serialize("json", data), content_type="application/json")
